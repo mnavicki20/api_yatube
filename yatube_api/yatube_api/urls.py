90abc2d67a2e4111885035a3f41c12c211ab05e5
Mikita Navicki <bulbanik91@gmail.com>
2022-01-17 18:47:17 +0300

    Добавлена аутентификация по токену TokenAuthentication

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,10 +1,13 @@
+from rest_framework.authtoken import views
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
 
 
