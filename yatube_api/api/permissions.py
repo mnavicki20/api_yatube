e1b84d5e03441b80d66a70165b146f5d5817c2a9
Mikita Navicki <bulbanik91@gmail.com>
2022-01-21 07:59:04 +0300

    Pytests пройдены

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
     """Проверка наличия прав редактирования и удаления объектов."""
 
-    def permission_to_object(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsAuthorOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from posts.models import Group, Post
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 from rest_framework.generics import get_object_or_404
@@ -10,7 +10,7 @@ from rest_framework import status
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
     def get_queryset(self):
         queryset = Post.objects.all()
@@ -25,24 +25,19 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         if serializer.instance.author != self.request.user:
             raise Response(status=status.HTTP_403_FORBIDDEN)
-        super(PostViewSet, self).perform_update(serializer) 
-
-    def perform_destroy(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise Response(status=status.HTTP_403_FORBIDDEN)
-        super(PostViewSet, self).perform_destroy(serializer) 
+        super(PostViewSet, self).perform_update(serializer)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     """Вьюсет для групп."""
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
